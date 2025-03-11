dcc1b9c1fcf16a798b0fbc94a832ffb122e3176e
personal-alizainkashif <alizainkashif773@gmail.com>
2025-03-11 15:33:21 +0500

    discount apply based on line item attribute key discount

--- extensions/product-discount-extension/src/run.js
+++ extensions/product-discount-extension/src/run.js
@@ -4,7 +4,6 @@ import { DiscountApplicationStrategy } from "../generated/api";
 /**
  * @typedef {import("../generated/api").RunInput} RunInput
  * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
- * @typedef {import("../generated/api").Target} Target
  */
 
 const EMPTY_DISCOUNT = {
@@ -17,26 +16,11 @@ const EMPTY_DISCOUNT = {
  * @returns {FunctionRunResult}
  */
 export function run(input) {
-  const discounts = [];
 
-  input.cart.lines.forEach((line) => {
-    // @ts-ignore
-    const metafield = line.merchandise?.product?.metafield;
-    if (metafield && metafield.value) {
-      const discountValue = parseFloat(metafield.value);
-      if (!isNaN(discountValue) && discountValue > 0) {
-        discounts.push({
-          targets: [{ cartLine: { id: line.id } }],
-          value: { percentage: { value: discountValue.toString() } },
-        });
-      }
-    }
-  });
-
-  if (discounts.length === 0) {
-    console.error("No eligible products for discount.");
-    return EMPTY_DISCOUNT;
-  }
+  const discounts = input.cart.lines.map(line => ({
+    targets: [{ cartLine: { id: line.id } }],
+    value: { percentage: { value: line.attribute?.value } },
+  }));
 
   return {
     discounts,
